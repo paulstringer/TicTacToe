75527e1b8ee413f479d58e66737277f39a7421ba
Paul Stringer <paulstringer@mac.com>
2016-02-22 15:50:43 +0000

    Moves repetition of ready() to test setup

--- TicTacToeTests/GameTypeHumanVersusHumanTests.swift
+++ TicTacToeTests/GameTypeHumanVersusHumanTests.swift
@@ -9,22 +9,20 @@ class GameTypeHumanVersusHumanTests: XCTestCase {
     override func setUp() {
         super.setUp()
         game = TicTacToe(view: view)
+        game.ready()
     }
 
     //MARK:- Humam V Human Tests
     
     func testGivenGame_WhenReady_ThenViewsGameTypesNotEmpty() {
-        game.ready()
         XCTAssertFalse(view.gameTypes.isEmpty)
     }
     
     func testGivenGame_WhenReady_ThenViewsGameTypesContainsHumanHuman() {
-        game.ready()
         XCTAssertTrue(view.gameTypes.contains(.HumanVersusHuman))
     }
  
     func testGivenGame_WhenTakingTurns_ThenViewsPlayerUpAlternates() {
-        game.ready()
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
         game.takeTurn(1, column: 1)
         XCTAssertEqual(view.gameState, GameState.PlayerTwoUp)
@@ -39,25 +37,21 @@ class GameTypeHumanVersusHumanTests: XCTestCase {
     }
     
     func testGivenGame_WhenTakingTurnWhereRowIsLessThanOne_ThenViewsPlayerRemainsPlayerOne() {
-        game.ready()
         game.takeTurn(0, column: 1)
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
     
     func testGivenGame_WhenTakingTurnWhereRowIsGreaterThanThree_ThenViewsPlayerRemainsPlayerOne() {
-        game.ready()
         game.takeTurn(4, column: 1)
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
     
     func testGivenGame_WhenTakingTurnWhereColumnIsLessThanOne_ThenViewsPlayerRemainsPlayerOne() {
-        game.ready()
         game.takeTurn(1, column: 0)
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
     
     func testGivenGame_WhenTakingTurnWhereColumnIsGreaterThanThree_ThenViewsPlayerRemainsPlayerOne() {
-        game.ready()
         game.takeTurn(1, column: 4)
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
