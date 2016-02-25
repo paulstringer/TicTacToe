ad59331d32be0e816bd038b340cb5773c2877704
Paul Stringer <paulstringer@mac.com>
2016-02-25 13:31:49 +0000

    Human v Computer test tidy up

--- TicTacToeTests/HumanVersusComputerTests.swift
+++ TicTacToeTests/HumanVersusComputerTests.swift
@@ -13,11 +13,13 @@ class HumanVersusComputerTests: XCTestCase {
         game.gameType = .HumanVersusComputer
     }
     
+    //MARK:- Humam V Human Tests
+    
     func testGivenGame_WhenReady_ThenViewsGameTypesContainsHumanVersusComputer() {
         XCTAssertTrue(view.gameTypes.contains(.HumanVersusComputer))
     }
     
-    func testGivenGame_WhenSelectingGameTypeHumanVersusComputer_ThenPlayerOneUp() {
+    func testGivenGame_WhenReady_ThenPlayerOneUp() {
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
     
@@ -25,37 +27,42 @@ class HumanVersusComputerTests: XCTestCase {
         XCTAssertNil(view.gameBoard.lastTurn)
     }
     
+    func testGivenGame_WhenReady_ThenPlayerOneIsUp() {
+        XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
+    }
+    
+    //MARK:- Turn taking Tests
+    
     func testGivenGame_WhenTakingTurn_ThenPlayerOneIsUp() {
         game.takeTurnAtPosition(0)
         XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
     
-    func testGivenGame_WhenTakingTurn_ThenComputerTakesTurn() {
+    func testGivenGame_WhenTakingTurn_ThenComputerTookATurn() {
         game.takeTurnAtPosition(.TopLeft)
         XCTAssertNotNil(view.gameBoard.lastTurn)
         XCTAssertNotEqual(view.gameBoard.lastTurn, .TopLeft)
     }
     
-    func testGivenGame_WhenTakingTurnAtPositionOne_ThenComputerTakesTurnAtDifferentPosition() {
+    func testGivenGame_WhenTakingTurnAtSecondPosition_ThenComputerTakesTurnAtDifferentPosition() {
         game.takeTurnAtPosition(1)
         XCTAssertNotEqual(view.gameBoard.lastTurn, BoardPosition(rawValue: 1))
     }
     
-    func testGivenGame_WhenTakingTwoTurns_ThenComputerTakesTurnsAtDifferentPosition() {
+    func testGivenGame_WhenTakingSecondTurn_ThenComputersSecondTurnIsAtDifferentPosition() {
         game.takeTurnAtPosition(0)
         let position = view.gameBoard.emptyPositions.first!
         game.takeTurnAtPosition(position)
         XCTAssertNotEqual(view.gameBoard.lastTurn, position)
-        XCTAssertEqual(view.gameState, GameState.PlayerOneUp)
     }
     
-    func testGivenGame_WhenTakingTurn_ThenBoardHasOneCrossOneNought() {
+    func testGivenGame_WhenTakingTurn_ThenBoardHasOneCrossAndOneNought() {
         game.takeTurnAtPosition(0)
         XCTAssertEqual(view.gameBoard.noughts, 1)
         XCTAssertEqual(view.gameBoard.crosses, 1)
     }
     
-    func testGivenGame_WhenTakingTwoTurns_ThenBoardHasTwoCrossesTwoNoughts() {
+    func testGivenGame_WhenTakingTwoTurns_ThenBoardHasTwoCrossesAndTwoNoughts() {
         game.takeTurnAtPosition(0)
         game.takeTurnAtPosition(view.gameBoard.emptyPositions.first!)
         XCTAssertEqual(view.gameBoard.noughts, 2)
