1e85fed360ac5c2ebc897e246754b50e60a950a3
Paul Stringer <paulstringer@mac.com>
2016-03-31 16:14:10 +0100

    Adds buttons to a game board in initial view controller

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -1,7 +1,20 @@
 import UIKit
 
+typealias ButtonPositions = [UIButton]
+
 class ViewController: UIViewController {
 
+    var buttons: ButtonPositions!
+    @IBOutlet var topLeftButton: UIButton!
+    @IBOutlet var topMiddleButton: UIButton!
+    @IBOutlet var topRightButton: UIButton!
+    @IBOutlet var middleLeftButton: UIButton!
+    @IBOutlet var middleButton: UIButton!
+    @IBOutlet var middleRightButton: UIButton!
+    @IBOutlet var bottomLeftButton: UIButton!
+    @IBOutlet var bottomMiddleButton: UIButton!
+    @IBOutlet var bottomRightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
